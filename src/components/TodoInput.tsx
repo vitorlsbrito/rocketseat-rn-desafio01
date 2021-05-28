ef8f1e055d3a50073cdbccd8a3a54f59e53ccf6d
Vitor Brito <vitorlsbrito@gmail.com>
2021-05-28 14:20:47 -0300

    Final commit

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image, Platform, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 
 import checkIcon from '../assets/icons/Check.png';
@@ -8,10 +8,12 @@ interface TodoInputProps {
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
   function handleAddNewTask() {
-    //TODO - Call addTask and clean input value 
+    //TODO - Call addTask and clean input value
+    addTask(task);
+    setTask('');
   }
 
   return (
@@ -20,12 +22,16 @@ export function TodoInput({ addTask }: TodoInputProps) {
         style={styles.input} 
         placeholder="Adicionar novo todo..."
         returnKeyType="send"
+        value={ task }
+        onChangeText={ setTask }
+        onSubmitEditing={ handleAddNewTask }
         //TODO - use value, onChangeText and onSubmitEditing props
       />
       <TouchableOpacity
         testID="add-new-task-button"
         activeOpacity={0.7}
         style={styles.addButton}
+        onPress={ handleAddNewTask }
         //TODO - onPress prop
       >
         <Image source={checkIcon} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,25 +11,40 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if (newTaskTitle.length > 0) {
+      const data = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+
+      setTasks([...tasks, data]);
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+    let taskList = [...tasks];
+    const taskId = taskList.findIndex((task) => task.id === id);
+    taskList[taskId].done = !taskList[taskId].done;
+
+    setTasks(taskList);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
   return (
     <>
       <Header />
 
-      <TodoInput addTask={handleAddTask} />
+      <TodoInput addTask={ handleAddTask } />
 
       <MyTasksList 
         tasks={tasks} 
